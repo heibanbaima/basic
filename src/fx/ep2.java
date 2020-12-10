d95d43256832591a23187da50e62ea5b2056eef0
heibanbaima <18610592047@163.com>
2020-12-10 17:00:17 +0800

    泛型

---
+++ src/fx/ep2.java
@@ -0,0 +1,16 @@
+package fx;
+
+public class ep2 <T>{
+    private T first;
+    private T last;
+    public ep2(T first,T last){
+        this.first=first;
+        this.last=last;
+    }
+    public T getFirst(){
+        return first;
+    }
+    public T getLast(){
+        return last;
+    }
+}
---
+++ src/fx/ep3.java
@@ -0,0 +1,16 @@
+package fx;
+
+public class ep3<T,K> {
+    private T first;
+    private K last;
+    public ep3(T first,K last){
+        this.first=first;
+        this.last=last;
+    }
+    public T getFirst(){
+        return first;
+    }
+    public K getLast(){
+        return last;
+    }
+}
